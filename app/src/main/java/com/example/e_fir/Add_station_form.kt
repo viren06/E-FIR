3528553bf1a757e2742b9fe7dc8c23d75693e541
virenpatel <viren000p@gmail.com>
2023-01-02 16:03:39 +0530

    super admin , and add station

---
+++ app/src/main/java/com/example/e_fir/Add_station_form.kt
@@ -0,0 +1,11 @@
+package com.example.e_fir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_station_form : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_station_form)
+    }
+}
--- app/src/main/java/com/example/e_fir/Superadmin_dashboard.kt
+++ app/src/main/java/com/example/e_fir/Superadmin_dashboard.kt
@@ -1,11 +1,22 @@
 package com.example.e_fir
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Superadmin_dashboard : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_superadmin_dashboard)
+
+        val add_station=findViewById<Button>(R.id.button2)
+
+        add_station.setOnClickListener{
+            startActivity(Intent(this,Add_station_form::class.java))
+
+        }
     }
 }
