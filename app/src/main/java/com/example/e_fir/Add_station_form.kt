29f4185aedf4f88240fda7e0453bb44e0894dcde
virenpatel <viren000p@gmail.com>
2023-01-04 14:28:00 +0530

    admin registration front end done and started working on back end.

--- app/src/main/java/com/example/e_fir/Add_station_form.kt
+++ app/src/main/java/com/example/e_fir/Add_station_form.kt
@@ -1,11 +1,23 @@
 package com.example.e_fir
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class Add_station_form : AppCompatActivity() {
+    private var filePath: Uri? = null
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+    private var username:String?=null
+    private var flg:String="false"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_add_station_form)
     }
 }
